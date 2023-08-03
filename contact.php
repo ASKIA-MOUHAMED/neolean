30eb30a9c067b681cd8f3262943c6094767e823d
Elhadji Mbaye BITEYE <59603967+elhadjibiteye@users.noreply.github.com>
2023-08-03 09:47:05 +0000

    commit1 du 03 Aout

---
+++ contact.php
@@ -0,0 +1,37 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nom_complet = $_POST["nom"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    // Mettez ici votre adresse e-mail où vous souhaitez recevoir les messages
+    $destinataire = "embiteye01@gmail.com";
+
+    $sujet = "Formulaire de contact neolean academy";
+
+    $contenu = "Nom Complet: " . $nom_complet . "\n";
+    $contenu .= "E-mail: " . $email . "\n";
+    $contenu .= "Message: \n" . $message;
+
+    // Vous pouvez ajouter des en-têtes supplémentaires pour spécifier le format du message, l'expéditeur, etc.
+    $headers = "From: " . $email . "\r\n";
+
+    // Envoyer l'e-mail
+    $envoi_email = mail($destinataire, $sujet, $contenu, $headers);
+
+    // Préparer la réponse JSON
+    $response = array();
+    if ($envoi_email) {
+        $response['success'] = true;
+        $response['message'] = "Votre message a été envoyé avec succès.";
+    } else {
+        $response['success'] = false;
+        $response['message'] = "Une erreur s'est produite lors de l'envoi du message.";
+    }
+
+    // Envoyer la réponse JSON
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
+?>
+
