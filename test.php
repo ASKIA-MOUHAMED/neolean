208b815b7d98a86f9745e1eddad75397a399c218
Elhadji Mbaye BITEYE <59603967+elhadjibiteye@users.noreply.github.com>
2023-08-08 14:02:22 +0000

    commitday1

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: https://neolean.digital"); // Remplacez par le domaine de "neoleanacademy.html"
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
-phpinfo();
-?>
