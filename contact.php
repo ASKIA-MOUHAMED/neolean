836a242e135ed68016152bc0a5524a4fc487bc10
Elhadji Mbaye BITEYE <59603967+elhadjibiteye@users.noreply.github.com>
2023-08-14 16:15:17 +0000

    push124

--- contact.php
+++ contact.php
@@ -1,41 +1,23 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nom_complet = $_POST["nom"];
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $email = $_POST["email"];
-    $message = $_POST["message"];
+    $nom = $_POST["nom"];
+    $messageText = $_POST["message"];
 
-    // Mettez ici votre adresse e-mail où vous souhaitez recevoir les messages
-    $destinataire = "embiteye01@gmail.com";
+    // Envoi de l'e-mail
+    $to = "embiteye01@gmail.com"; // Remplacez par l'adresse e-mail du destinataire
+    $subject = "Formulaire de contact Neolean";
+    $message = "Un nouveau message a été envoyé :\n\n";
+    $message .= "Nom : $nom\n";
+    $message .= "E-mail : $email\n";
+    $message .= "Message : $messageText\n";
 
-    $sujet = "Formulaire de contact neolean academy";
-
-    $contenu = "Nom Complet: " . $nom_complet . "\n";
-    $contenu .= "E-mail: " . $email . "\n";
-    $contenu .= "Message: \n" . $message;
-
-    // Vous pouvez ajouter des en-têtes supplémentaires pour spécifier le format du message, l'expéditeur, etc.
-    $headers = "From: " . $email . "\r\n";
+    $headers = "From: innovation@neolean.digital"; // Remplacez par l'adresse e-mail de l'expéditeur
 
     // Envoyer l'e-mail
-    $envoi_email = mail($destinataire, $sujet, $contenu, $headers);
+    $success = mail($to, $subject, $message, $headers);
 
-    // Préparer la réponse JSON
-    $response = array();
-    if ($envoi_email) {
-        $response['success'] = true;
-        $response['message'] = "Votre message a été envoyé avec succès.";
-    } else {
-        $response['success'] = false;
-        $response['message'] = "Une erreur s'est produite lors de l'envoi du message.";
-    }
-
-    // Envoyer la réponse JSON
-    header('Content-Type: application/json');
-    echo json_encode($response);
+    // Retourner une réponse JSON
+    echo json_encode(["success" => $success]);
 }
 ?>
-
